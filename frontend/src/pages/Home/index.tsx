f35e6895b763475247e6f436ebe5e690fb011eff
gedom2012 <gedom2012@gmail.com>
2021-11-06 11:51:06 +0000

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,24 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div >
-            <h1>Página Home</h1>
-        </div>
-
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DS-Sales</h1>
+                    <p className="lead">Analisys the sales performance by different perspectives</p>
+                    <hr />
+                    <p>This application consist in the show the dashboard from data given by API constructed with Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Access Dashboard
+                    </Link>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
-
 export default Home;
