f056b764850e5503703ad4c6890c9faf6f1a263f
gedom2012 <gedom2012@gmail.com>
2021-11-07 02:06:01 +0000

    Update personal link

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App developed by <a href="https://github.com/gedom2012" target="_blank" rel="noreferrer">Marcelo Mariath</a></p>
+                <p className="text-light">App developed by <a href="https://www.linkedin.com/in/marcelo-mariath-13a638b0" target="_blank" rel="noreferrer">Marcelo Mariath</a></p>
                 <p className="text-light"><small><strong>Spring React Week</strong><br />
                     Promoted by the DevSuperior School: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
